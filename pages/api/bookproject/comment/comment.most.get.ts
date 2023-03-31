07c5cd40871dea3bfb2d5bc04c680b75f8ff0843
Lenson-Lee <dmstjs7437@naver.com>
2023-03-31 15:13:04 +0900

    코멘트 DB 수정

--- pages/api/bookproject/comment/comment.most.get.ts
+++ pages/api/bookproject/comment/comment.most.get.ts
@@ -10,9 +10,12 @@ export default function List({ data }: Props) {
 }
 
 export async function getComment() {
+  //인기순 코멘트 조회
   const document = await prisma.comment.findMany({
     orderBy: [{ like: "desc" }],
   });
+
+  //해당 책 코멘트별 책 제목 조회
 
   const data = { document };
   console.log(">comment.most.get 끝 --END");
